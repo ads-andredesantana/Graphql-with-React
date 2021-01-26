b533042d9327dd47e59c54bca1c54da4b7d16b59
ads-andredesantana <ads-andredesantana@gmail.com>
2021-01-26 06:50:41 -0500

    Defining root for books

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require('graphql');
+const_ = require('lodash');
 
 const { GraphqlObjectType, GraphQLString, GraphQlSchema } = graphql;
+
+// Dummy data to start and test
+var books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
 
 // Describing the Book
 const BookType = new GraphqlObjectType({
@@ -21,7 +29,7 @@ const RootQuery = new GraphqlObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         // Code to get data from db / other source
-
+        return _.find(books, { id: args.id });
       }
     }
   }
