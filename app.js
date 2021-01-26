bc60479f49830d08d55fd43cf9d880e12b0bf638
ads-andredesantana <ads-andredesantana@gmail.com>
2021-01-26 13:26:28 -0500

    Creating connection to Mongo DB

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+
+// Creating connection with MongoDB
+mongoose.connect('mongodb+srv://fernandes30:$Bootcamp123@react-gql.uau7a.mongodb.net/react-gql?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
+
 
 // Middleware to handle the function and create the Schema
 app.use('/graphql', graphqlHTTP({
@@ -13,5 +22,4 @@ app.use('/graphql', graphqlHTTP({
 
 app.listen(4000, () => {
   console.log('Listening on Port 4000');
-
 });
