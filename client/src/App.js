e73dfb8eef7da927cfb30a3dcf8766b3dceca04c
ads-andredesantana <ads-andredesantana@gmail.com>
2021-01-27 13:07:47 -0500

    Creating Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+
+
 // Components
 import Booklist from './components/BookList'
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
+
 function App() {
   return (
-    <div id="main">
-      <h1>Ninja's Reading List</h1>
-      <Booklist />
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Ninja's Reading List</h1>
+        <Booklist />
+      </div>
+    </ApolloProvider>
   );
 }
 
